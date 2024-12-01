d8b973e2997e726a1abe303013bdad7e4bf2760e
PUTAXD <indraarvy12@gmail.com>
2024-12-01 20:24:40 +0700

    db_kue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,69 +1,28 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 function App() {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("http://localhost:8080/categories")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((error) => console.error(error))
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <div className="container">
-      <div className="row">
-        <h1 className="mt-3"> Go react Movie Project!</h1>
-        <hr className="mb-3" />
-      </div>
-
-      <div className="row">
-        <div className="col-2">
-          <ul className="list-group">
-            <li className="list-group-item">An item</li>
-            <li className="list-group-item">A second item</li>
-            <li className="list-group-item">A third item</li>
-            <li className="list-group-item">A fourth item</li>
-            <li className="list-group-item">And a fifth one</li>
-          </ul>
-        </div>
-        <div className="col-10">
-          <div className="row">
-            <div className="col-sm-4 mb-3 mb-sm-0">
-              <div className="card">
-                <div className="card-body">
-                  <h5 className="card-title">Special title treatment</h5>
-                  <p className="card-text">
-                    With supporting text below as a natural lead-in to
-                    additional content.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-4">
-              <div className="card">
-                <div className="card-body">
-                  <h5 className="card-title">Special title treatment</h5>
-                  <p className="card-text">
-                    With supporting text below as a natural lead-in to
-                    additional content.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </div>
-            <div className="col-sm-4 ">
-              <div className="card">
-                <div className="card-body">
-                  <h5 className="card-title">Special title treatment</h5>
-                  <p className="card-text">
-                    With supporting text below as a natural lead-in to
-                    additional content.
-                  </p>
-                  <a href="#" className="btn btn-primary">
-                    Go somewhere
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <ul>
+          {data.map((category) => (
+            <li key={category.Id}>{category.Name}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
