5d4ea70edfc158dbe8a35a5f8b9d85e470f4fe94
PUTAXD <indraarvy12@gmail.com>
2024-12-03 23:27:13 +0700

    CORS with Golang

--- server/config/config.go
+++ server/config/config.go
@@ -10,7 +10,7 @@ import (
 var DB *sql.DB
 
 func ConnectDB() {
-	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3310)/db_kue?parseTime=True")
+	db, err := sql.Open("mysql", "admin:root@/db_kue?parseTime=True")
 	if err != nil {
 		panic(err)
 	}
--- server/main.go
+++ server/main.go
@@ -6,39 +6,45 @@ import (
 	"server/api/apisupplier"
 	"server/api/apivariant"
 	"server/config"
+	"time"
 
-  "github.com/gin-contrib/cors"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 
-  func main() {
-      config.ConnectDB()
-      
-
-      r := gin.Default()
-
-      // Configure CORS
-      r.Use(cors.Default())
-
-      //varian
-      r.GET("/varians/all", apivariant.GetVarian)
-
-      //supplier
-      r.GET("/supplier/all", apisupplier.GetSupplier)
-
-      //kue
-      r.GET("/kue/all", apikue.GetKue)
-      r.GET("/kue/:id", apikue.DetailKue)
-      r.POST("/kue/add", apikue.AddKue)
-      r.PUT("/kue/update/:id", apikue.UpdateKue)
-      r.DELETE("/kue/delete/:id", apikue.DeleteKue)
-
-
-      if err := r.Run(":8080"); err != nil {
-      log.Fatal("Server failed to start:", err)
-    }
-
-
+func main() {
+    config.ConnectDB()
     
-  }
+
+    r := gin.Default()
+    r.Use(cors.New(cors.Config{
+      AllowOrigins:     []string{"*"}, // Membuka akses untuk semua origin
+      AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+      AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+      AllowCredentials: true,
+      ExposeHeaders:    []string{"Content-Length"},
+      MaxAge:           12 * time.Hour,
+    }))
+  
+    //varian
+    r.GET("/varians/all", apivariant.GetVarian)
+
+    //supplier
+    r.GET("/supplier/all", apisupplier.GetSupplier)
+
+    //kue
+    r.GET("/kue/all", apikue.GetKue)
+    r.GET("/kue/:id", apikue.DetailKue)
+    r.POST("/kue/add", apikue.AddKue)
+    r.PUT("/kue/update/:id", apikue.UpdateKue)
+    r.DELETE("/kue/delete/:id", apikue.DeleteKue)
+
+
+    if err := r.Run(":8080"); err != nil {
+		log.Fatal("Server failed to start:", err)
+	}
+
+
+
+}
